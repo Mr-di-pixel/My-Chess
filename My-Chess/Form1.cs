69548cbf0bd74fd9a8f6aa94f1a24e9ab299705f
Mr-di-pixel <mrdi930628@gmail.com>
2025-05-19 23:44:14 +0300

    Создали поле и кнопки на нём, прописали нахождение спрайта фигур, и метод для вырезания и добавления

--- My-Chess/Form1.cs
+++ My-Chess/Form1.cs
@@ -1,15 +1,63 @@
+using Microsoft.VisualBasic.ApplicationServices;
+
 namespace My_Chess
 {
     public partial class Form1 : Form
     {
+        public Image chessSprites;
+        public int[,] map = new int[8, 8]
+        {
+            {15,14,13,12,11,13,14,15},
+            {16,16,16,16,16,16,16,16},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0},
+            {26,26,26,26,26,26,26,26},
+            {25,24,23,22,21,23,24,25},
+        };
         public Form1()
         {
             InitializeComponent();
+
+            chessSprites = new Bitmap("C:\\Users\\mrdi9\\source\\repos\\My-Chess\\My-Chess\\chess.png");
+           
+          //  button1.BackgroundImage = part;
+
+            Init();
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        public void Init()
         {
+          CreateMap();
+        }
+        public void CreateMap()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++) 
+                { 
+                    Button butt=new Button();
+                    butt.Size = new Size(50,50);
+                    butt.Location = new Point (y * 50, x * 50);
+                    switch (map[x, y]/10)
+                    {
+                        case 1:
+                            Image part = new Bitmap(50, 50);
+                            Graphics g = Graphics.FromImage(part);
+                            g.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0 + 150 * (map[x,y]%10-1), 0, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part;
+                            break;
+                            case 2:
+                            Image part1 = new Bitmap(50, 50);
+                            Graphics g1 = Graphics.FromImage(part1);
+                            g1.DrawImage(chessSprites, new Rectangle(0, 0, 50, 50), 0 + 150 * (map[x, y] % 10-1), 150, 150, 150, GraphicsUnit.Pixel);
+                            butt.BackgroundImage = part1;
+                            break;
+                    }
+                    this.Controls.Add(butt);
+                }
 
+            }
         }
     }
 }
