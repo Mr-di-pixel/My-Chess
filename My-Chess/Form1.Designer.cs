fb0b91dc7543bfe68ead1c5f4bf209b5a0d5d6a0
Mr-di-pixel <mrdi930628@gmail.com>
2025-06-01 13:07:09 +0300

    Добавленна ветвь с функционалом Доп.Кнопок. (Добавленна кнопка РЕСТАРТ)

--- My-Chess/Form1.Designer.cs
+++ My-Chess/Form1.Designer.cs
@@ -15,18 +15,33 @@
         #region Windows Form Designer generated code
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(404, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(74, 42);
+            button1.TabIndex = 0;
+            button1.Text = "Restart";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(432, 403);
+            ClientSize = new Size(482, 403);
+            Controls.Add(button1);
             Name = "Form1";
             Text = "My-Chess";
+            Load += Form1_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
